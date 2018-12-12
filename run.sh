4ff52a01598fee72014aae495dfe0eaded9494e1
LachubCz <petr.genius.buchal@gmail.com>
2018-12-12 14:32:55 +0100

    Merge fix.

--- run.sh
+++ run.sh
@@ -2,14 +2,24 @@
 
 run_type=$1
 
+if [ "$run_type" = "--remake" ]; then
+  rm -r build
+  rm sample_scene.png
+  rm render_scene.png
+  cmake -H. -Bbuild
+  make -C build
+  build/raytracer sample_scene.txt
+fi
 if [ "$run_type" = "--make" ]; then
-	cmake -H. -Bbuild
-	make -C build
-	build/raytracer sample_scene.txt
+  cmake -H. -Bbuild
+  make -C build
+  build/raytracer
 fi
 if [ "$run_type" = "--run" ]; then
-  	build/raytracer sample_scene.txt
+  build/raytracer
 fi
 if [ "$run_type" = "--clear" ]; then
   rm -r build
+  rm sample_scene.png
+  rm render_scene.png
 fi
--- src/quadric.cpp
+++ src/quadric.cpp
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "quadric.h"
 #include "stdio.h"
 
